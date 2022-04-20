e8f567806070fbd4f0dac9e11da603a30966fc42
gabodmc <gabrielmc@live.com.ar>
2022-04-20 11:16:31 -0300

    env example & sequelizerc fix

--- database/config/config.js
+++ database/config/config.js
@@ -4,32 +4,32 @@ module.exports =
 
 { 
     development: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
+        "dialect": process.env.DB_DIALECT,
 
-        seederStorage: sequelize,
-        seederStorageTableName: seeds,
+        seederStorage: "sequelize",
+        seederStorageTableName: "seeds",
 
-        migrationStorage: sequelize,
-        migrationStorageTableName: migrations
+        migrationStorage: "sequelize",
+        migrationStorageTableName: "migrations"
     },
 
     production: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "port": process.env.DB_PORT,
+        "dialect": process.env.DB_DIALECT,
     
-        seederStorage: sequelize,
-        seederStorageTableName: seeds,
+        seederStorage: "sequelize",
+        seederStorageTableName: "seeds",
     
-        migrationStorage: sequelize,
-        migrationStorageTableName: migrations
+        migrationStorage: "sequelize",
+        migrationStorageTableName: "migrations"
     }
 }
